c8ad93dbdf0878b5ff600c9d31552a482286e158
ammandzik <preisner.amanda@gmail.com>
2024-03-07 19:27:29 +0100

    Changes commit

--- src/Main.java
+++ src/Main.java
@@ -31,9 +31,6 @@ public class Main {
 
         boolean isPlaying = true;
         int initialScore = 10;
-        int countWins = 0;
-
-
 
         printWord(title, playerGuesses);
 
@@ -57,7 +54,6 @@ public class Main {
             }
             }
         }
-
 
         public static boolean getPlayerGuesses (Scanner userChoice, String title, List <Character> playerGuesses) {
 
